ce48f31388310fadc8fcbe76436e73927e490e4d
Jose Carlos <cpalacin@syscover.com>
2015-05-26 21:09:42 +0200

    seeds

--- src/database/seeds/ResourceFormsTableSeeder.php
+++ src/database/seeds/ResourceFormsTableSeeder.php
@@ -9,11 +9,11 @@ class ResourceFormsTableSeeder extends Seeder {
     {
         Resource::insert([
             ['id_007' => 'forms','name_007' => 'Forms Package','package_007' => '4'],
+            ['id_007' => 'forms-mail-account','name_007' => 'Mail accounts','package_007' => '4'],
+            ['id_007' => 'forms-state','name_007' => 'States','package_007' => '4'],
             ['id_007' => 'forms-form','name_007' => 'Forms','package_007' => '4'],
-            ['id_007' => 'forms-vironment','name_007' => 'Records','package_007' => '4'],
-            ['id_007' => 'forms-decoration','name_007' => 'Decorations','package_007' => '4'],
-            ['id_007' => 'forms-relationship','name_007' => 'Relationship','package_007' => '4'],
-            ['id_007' => 'forms-service','name_007' => 'Services','package_007' => '4']
+            ['id_007' => 'forms-record','name_007' => 'Records','package_007' => '4'],
+            ['id_007' => 'forms-comment','name_007' => 'Comments','package_007' => '4']
         ]);
     }
 }
