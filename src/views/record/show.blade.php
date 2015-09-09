047a624e2330c2b517e80615c19c05c1f44125f6
Jose Carlos <cpalacin@syscover.com>
2015-09-09 19:17:46 +0200

    minor fixed

--- src/views/record/show.blade.php
+++ src/views/record/show.blade.php
@@ -117,9 +117,6 @@
         @if(isset($object->email_403))
             @include('pulsar::includes.html.form_text_group', ['label' => trans('pulsar::pulsar.email'), 'name' => 'email', 'value' => $object->email_403, 'fieldSize' => 5, 'readOnly' => true])
         @endif
-        @if(isset($object->date_403))
-            @include('pulsar::includes.html.form_text_group', ['label' => trans('pulsar::pulsar.date'), 'name' => 'date', 'value' => date('d-m-Y', $object->date_403), 'fieldSize' => 4, 'readOnly' => true])
-        @endif
         @foreach(json_decode($object->data_403) as $field)
             @if($field->type == 'text' || $field->type == 'select-one'|| $field->type == 'select-multiple' || $field->type == 'hidden' || $field->type == 'tel' || $field->type == 'email')
                 @include('pulsar::includes.html.form_text_group', ['label' => isset($field->label)? $field->label : ucfirst($field->name), 'name' => '', 'value' => $field->value, 'fieldSize' => isset($field->length)? $field->length : 10 , 'readOnly' => true])
