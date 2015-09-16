78be2881e3db6acd82f06339b70697eab54fac3b
Jose Carlos <cpalacin@syscover.com>
2015-09-16 19:51:16 +0200

    change name of record

--- src/Syscover/Forms/Controllers/RecordController.php
+++ src/Syscover/Forms/Controllers/RecordController.php
@@ -32,7 +32,7 @@ class RecordController extends Controller {
     protected $folder       = 'record';
     protected $package      = 'forms';
     protected $aColumns     = ['id_403', ['type' => 'color_400', 'data' => 'name_400', 'tooltip' => true, 'title' => 'name_400'], 'date_403', 'date_text_403', 'name_403', 'surname_403', ['type' => 'email', 'data' => 'email_403'], ['type' => 'active', 'data' => 'opened_403']];
-    protected $nameM        = 'name_403';
+    protected $nameM        = 'id_403';
     protected $model        = '\Syscover\Forms\Models\Record';
     protected $icon         = 'icon-file-text-alt';
     protected $objectTrans  = 'record';
