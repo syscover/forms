0d5e2e8302fcd4bf117009c8bc40197e1a719fad
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-15 17:40:24 +0100

    minor fix

--- src/views/includes/nav/main.blade.php
+++ src/views/includes/nav/main.blade.php
@@ -1,21 +1,21 @@
-        <li{!! Miscellaneous::setCurrentOpenPage(['forms-state','forms-form','forms-record','forms-preference']) !!}>
-            <a href="javascript:void(0)"><i class="icomoon-icon-drawer-3"></i>Forms</a>
-            <ul class="sub-menu">
-                @if(session('userAcl')->allows('forms-form', 'access'))
-                    <li{!! Miscellaneous::setCurrentPage(['forms-form', 'forms-record']) !!}><a href="{{ route('formsForm') }}"><i class="fa fa-file-text-o"></i>{{ trans_choice('pulsar::pulsar.form', 2) }}</a></li>
+<li{!! Miscellaneous::setCurrentOpenPage(['forms-state','forms-form','forms-record','forms-preference']) !!}>
+    <a href="javascript:void(0)"><i class="icomoon-icon-drawer-3"></i>Forms</a>
+    <ul class="sub-menu">
+        @if(session('userAcl')->allows('forms-form', 'access'))
+            <li{!! Miscellaneous::setCurrentPage(['forms-form', 'forms-record']) !!}><a href="{{ route('formsForm') }}"><i class="fa fa-file-text-o"></i>{{ trans_choice('pulsar::pulsar.form', 2) }}</a></li>
+        @endif
+        @if(session('userAcl')->allows('forms-state', 'access') || session('userAcl')->allows('forms-preference', 'access'))
+        <li{!! Miscellaneous::setCurrentOpenPage(['forms-state','forms-preference']) !!}>
+            <a href="javascript:void(0)"><i class="icomoon-icon-grid"></i>{{ trans('pulsar::pulsar.master_tables') }}</a>
+            <ul class="sub-menu" >
+                @if(session('userAcl')->allows('forms-state', 'access'))
+                    <li{!! Miscellaneous::setCurrentPage('forms-state') !!}><a href="{{ route('formsState') }}"><i class="fa fa-fire"></i>{{ trans_choice('pulsar::pulsar.state', 2) }}</a></li>
                 @endif
-                @if(session('userAcl')->allows('forms-state', 'access') || session('userAcl')->allows('forms-preference', 'access'))
-                <li{!! Miscellaneous::setCurrentOpenPage(['forms-state','forms-preference']) !!}>
-                    <a href="javascript:void(0)"><i class="icomoon-icon-grid"></i>{{ trans('pulsar::pulsar.master_tables') }}</a>
-                    <ul class="sub-menu" >
-                        @if(session('userAcl')->allows('forms-state', 'access'))
-                            <li{!! Miscellaneous::setCurrentPage('forms-state') !!}><a href="{{ route('formsState') }}"><i class="fa fa-fire"></i>{{ trans_choice('pulsar::pulsar.state', 2) }}</a></li>
-                        @endif
-                        @if(session('userAcl')->allows('forms-preference', 'access'))
-                            <li{!! Miscellaneous::setCurrentPage('forms-preference') !!}><a href="{{ route('formsPreference') }}"><i class="fa fa-cog"></i>{{ trans_choice('pulsar::pulsar.preference', 2) }}</a></li>
-                        @endif
-                    </ul>
-                </li>
+                @if(session('userAcl')->allows('forms-preference', 'access'))
+                    <li{!! Miscellaneous::setCurrentPage('forms-preference') !!}><a href="{{ route('formsPreference') }}"><i class="fa fa-cog"></i>{{ trans_choice('pulsar::pulsar.preference', 2) }}</a></li>
                 @endif
             </ul>
         </li>
+        @endif
+    </ul>
+</li>
--- src/views/preference/index.blade.php
+++ src/views/preference/index.blade.php
@@ -5,7 +5,7 @@
     <!-- forms::preferences.index -->
     @include('pulsar::includes.js.success_message')
 
-    <script type="text/javascript">
+    <script>
         $(document).ready(function() {
             $(".custom-select2").select2({
                 templateResult: formatState,
@@ -23,12 +23,34 @@
             return $option;
         };
     </script>
-    <!-- /forms::preferences.index -->
+    <!-- ./forms::preferences.index -->
 @stop
 
 @section('rows')
     <!-- forms::preferences.index -->
-    @include('pulsar::includes.html.form_select_group', ['label' => trans_choice('pulsar::pulsar.state', 1), 'name' => 'defaultState', 'value' => (int)$defaultState->value_018, 'objects' => $states, 'idSelect' => 'id_400', 'nameSelect' => 'name_400', 'class' => 'form-control custom-select2', 'fieldSize' => 5, 'required' => true, 'dataOption' => ['color' => 'color_400']])
-    @include('pulsar::includes.html.form_select_group', ['label' => trans('forms::pulsar.notifications_account'), 'name' => 'notificationsAccount', 'value' => (int)$notificationsAccount->value_018, 'objects' => $accounts, 'idSelect' => 'id_013', 'nameSelect' => 'name_013', 'fieldSize' => 5, 'required' => true])
-    <!-- /forms::preferences.index -->
+    @include('pulsar::includes.html.form_select_group', [
+        'label' => trans_choice('pulsar::pulsar.state', 1),
+        'name' => 'defaultState',
+        'value' => (int)$defaultState->value_018,
+        'objects' => $states,
+        'idSelect' => 'id_400',
+        'nameSelect' => 'name_400',
+        'class' => 'form-control custom-select2',
+        'fieldSize' => 5,
+        'required' => true,
+        'dataOption' => [
+            'color' => 'color_400'
+        ]
+    ])
+    @include('pulsar::includes.html.form_select_group', [
+        'label' => trans('forms::pulsar.notifications_account'),
+        'name' => 'notificationsAccount',
+        'value' => (int)$notificationsAccount->value_018,
+        'objects' => $accounts,
+        'idSelect' => 'id_013',
+        'nameSelect' => 'name_013',
+        'fieldSize' => 5,
+        'required' => true
+    ])
+    <!-- ./forms::preferences.index -->
 @stop
