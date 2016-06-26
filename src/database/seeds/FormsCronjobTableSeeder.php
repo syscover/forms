f2cf3fe77e942eed9650dc32a7b070cbaa295960
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-06-26 02:20:24 +0200

    change package name

--- src/database/seeds/FormsCronjobTableSeeder.php
+++ src/database/seeds/FormsCronjobTableSeeder.php
@@ -8,7 +8,7 @@ class FormsCronjobTableSeeder extends Seeder {
     public function run()
     {   
         CronJob::insert([
-            ['name_011' => 'Check messages to send', 'package_011' => 4, 'cron_expression_011' => '*/5 * * * *', 'key_011' => '08', 'last_run_011' => 0, 'next_run_011' => 0, 'active_011' => 1]
+            ['name_011' => 'Check messages to send', 'package_id_011' => 4, 'cron_expression_011' => '*/5 * * * *', 'key_011' => '08', 'last_run_011' => 0, 'next_run_011' => 0, 'active_011' => 1]
         ]);
     }
 }
